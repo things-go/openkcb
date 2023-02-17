f020184657d35947d7cedee80179ae197b004a65
thinkgos <thinkgo@aliyun.com>
2023-02-17 13:54:37 +0800

    fix: add .

---
+++ business_kind.go
@@ -0,0 +1,16 @@
+package openkcb
+
+const (
+	//  账户类型
+	CustomerType_PERSONAL   = "60" // 个人
+	CustomerType_ENTERPRISE = "80" // 企业
+	// 客户证件类型
+	CustomerIdType_IDENTITY_CARD    = "1" // 身份证
+	CustomerIdType_BUSINESS_LICENSE = "2" // 营业执照/统一社会信用代码
+	// 货币代码
+	CurrencyCode_CNY = "CNY"
+	// 会员类型
+	MemberType_PURCHASER = 1 // 买家
+	MemberType_SELLER    = 2 // 卖家
+
+)
